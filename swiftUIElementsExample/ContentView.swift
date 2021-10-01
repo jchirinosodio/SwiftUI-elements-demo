e91f7556c4962951fa9e01fcd983fe9d78ec8e0f
Jose Eduardo Chirinos Odio <jchirinosodio@gmail.com>
2021-10-01 13:16:10 -0600

    Initial Commit

---
+++ swiftUIElementsExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIElementsExample
+//
+//  Created by Jose Chirinos Odio on 1/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIElementsExample/swiftUIElementsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIElementsExampleApp.swift
+//  swiftUIElementsExample
+//
+//  Created by Jose Chirinos Odio on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIElementsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
